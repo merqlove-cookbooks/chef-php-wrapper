6962d498d84d63c81aaf4721d81700da51ef0d29
Alexander Merkulov <sasha@merqlove.ru>
2017-04-10 12:37:52 +0300

    clean php

--- attributes/default.rb
+++
@@ -1,8 +0,0 @@
-if platform_family?('rhel')
-  default['php']['packages'] = %w(php56-php php56-php-devel php56-php-cli php-pear)
-  default['php']['mysql']['package'] = 'php-mysql'
-  default['php']['fpm_package']   = 'php-fpm'
-  default['php']['fpm_pooldir']   = '/etc/php-fpm.d'
-  default['php']['fpm_default_conf'] = '/etc/php-fpm.d/www.conf'
-  default['php']['fpm_service'] = 'php-fpm'
-end
