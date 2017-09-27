67458b9a60ba046591c28cf971b088c07f11f259
Alexander Merkulov <sasha@merqlove.ru>
2017-09-27 16:23:45 +0300

    use 4.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sasha@merqlove.ru'
 license 'all_rights'
 description 'Installs/Configures php'
 long_description 'Installs/Configures php'
-version '0.0.2'
+version '0.0.3'
 
-depends 'php', '4.5.0'
+depends 'php', '4.0'
